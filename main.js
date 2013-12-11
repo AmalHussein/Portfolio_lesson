40245ed90fc8144b66a4b99d7c79f013ef742fd3
Amal Hussein <amalhussein08@gmail.com>
2013-12-11 10:04:19 -0500

    saving changes to heroku deploy

---
+++ main.js
@@ -0,0 +1,23 @@
+var Page = {
+
+	setNavigationClicks: function() {
+		$('.navigation-wrapper a').click(function(e){
+			e.preventDefault();
+			Page.scrollToSection($(e.currentTarget).attr('href'));
+		});
+	}, 
+	scrollTo: function(yValue) {
+		$('html body').animate({
+			'scrollTop' :yValue 
+		}, 1000);
+	},
+	scrollToSection: function(sectionString) {
+		var sectionClass = "."+ sectionString;
+		Page.scrollTo($(sectionClass).offset().top - 100);
+	}
+};
+
+
+$(function() {
+	Page.setNavigationClicks();
+});
